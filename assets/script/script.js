60e6ad651741e84663d178e9841ea83dec9670d3
grambo <gramham@live.com>
2020-02-10 14:58:46 -0700

    added form and table

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,21 @@
 // console.log("testignlink");
 // $("#testjquery").html("testingjquesry");
+
+var config = {
+    apiKey: "AIzaSyA_QypGPkcjPtylRDscf7-HQl8ribnFeIs",
+    authDomain: "time-sheet-55009.firebaseapp.com",
+    databaseURL: "https://time-sheet-55009.firebaseio.com",
+    storageBucket: "time-sheet-55009.appspot.com"
+  };
+  
+  firebase.initializeApp(config);
+  var database = firebase.database();
+  console.log(database);
+
+// var newRow = $("<tr>").append(
+//     $("<td>").text(empName),
+//     $("<td>").text(empRole),
+//     $("<td>").text(empStartPretty),
+//     $("<td>").text(empMonths),
+//     $("<td>").text(empRate),
+//   );
